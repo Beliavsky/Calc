09759ed12b3252b0d2623138ad0c53e7497d3ea1
Beliavsky <beliavsky@aol.com>
2025-04-24 12:09:17 -0400

    tests ^ for exponentiation

--- xcalc.f90
+++ xcalc.f90
@@ -3,4 +3,7 @@ use calc_mod, only: eval_print
 implicit none
 call eval_print("10 + (5 * (1 + (2*3)))")
 call eval_print("1 + 2*3")
+call eval_print("3^2 + 4^2")
+call eval_print("(3^2 + 4^2)^0.5")
+call eval_print("(3^2 + 4^2)^(-0.5)")
 end program test_evaluator
