01dc2faac9597b20083b7b5180da8b4e8fc39264
Beliavsky <beliavsky@aol.com>
2025-04-25 18:40:13 -0400

    added eval_print loop

--- xinterpret.f90
+++ xinterpret.f90
@@ -1,10 +1,17 @@
 program xinterpret
   use interpret_mod
   implicit none
+  character (len=1000) :: line
   call eval_print("x = 3")
   call eval_print("y = [1, 2, 3]")
   call eval_print("z = y * x")
   call eval_print("w = [10 20 30] + y")
   call eval_print("z ^ 2")
+  do
+     write (*,"('> ')", advance="no")
+     read (*,"(a)") line
+     if (line == "exit") exit
+     call eval_print(trim(line))
+  end do
 end program xinterpret
 
