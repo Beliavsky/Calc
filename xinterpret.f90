dbab3289957ebda99302e3699042427f4f02026e
Beliavsky <beliavsky@aol.com>
2025-04-26 07:41:21 -0400

    added more examples

--- xinterpret.f90
+++ xinterpret.f90
@@ -2,15 +2,18 @@ program xinterpret
   use interpret_mod
   implicit none
   character (len=1000) :: line
-  call eval_print("x = 3")
+  call eval_print("n = 10")
   call eval_print("y = [1, 2, 3]")
-  call eval_print("z = y * x")
+  call eval_print("z = n * y")
   call eval_print("w = [10 20 30] + y")
   call eval_print("z ^ 2")
+  call eval_print("x = runif(n)")
+  call eval_print("sum(x)")
+  call eval_print("[sum(x) minval(x) maxval(x)]")
   do
      write (*,"('> ')", advance="no")
      read (*,"(a)") line
-     if (line == "exit") exit
+     if (line == "exit" .or. line == "exit()" .or. line == "quit" .or. line == "quit()" .or. line == "q" .or. line == "q()") exit
      call eval_print(trim(line))
   end do
 end program xinterpret
