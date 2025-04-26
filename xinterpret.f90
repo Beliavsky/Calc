6687a5f37b0facbf3f0fa89df394f6c960707176
Beliavsky <beliavsky@aol.com>
2025-04-26 07:54:08 -0400

    checks for end-of-file in code input

--- xinterpret.f90
+++ xinterpret.f90
@@ -1,6 +1,7 @@
 program xinterpret
   use interpret_mod
   implicit none
+  integer :: iostat_err
   character (len=1000) :: line
   call eval_print("n = 10")
   call eval_print("y = [1, 2, 3]")
@@ -12,7 +13,8 @@ program xinterpret
   call eval_print("[sum(x) minval(x) maxval(x)]")
   do
      write (*,"('> ')", advance="no")
-     read (*,"(a)") line
+     read (*,"(a)", iostat=iostat_err) line
+     if (iostat_err /= 0) exit
      if (line == "exit" .or. line == "exit()" .or. line == "quit" .or. line == "quit()" .or. line == "q" .or. line == "q()") exit
      call eval_print(trim(line))
   end do
