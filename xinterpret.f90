3f63857f2b90e4f712fd07a71a6c2111a19e10b9
Beliavsky <beliavsky@aol.com>
2025-04-26 19:44:39 -0400

    writes variables to file at end of program

--- xinterpret.f90
+++ xinterpret.f90
@@ -1,8 +1,10 @@
 program xinterpret
-  use interpret_mod, only: eval_print, tunit, code_transcript_file
+  use interpret_mod, only: eval_print, tunit, code_transcript_file, vars
   implicit none
-  integer :: iostat_err
+  integer :: i, iostat_err, varu
   character (len=1000) :: line
+  logical, parameter :: write_vars_at_end = .true.
+  character (len=*), parameter :: vars_file = "temp_vars.txt"
   open (newunit=tunit, file=code_transcript_file, action="write", status="replace")
   call eval_print("n = 10")
   call eval_print("y = [1, 2, 3]")
@@ -19,5 +21,12 @@ program xinterpret
      if (line == "exit" .or. line == "exit()" .or. line == "quit" .or. line == "quit()" .or. line == "q" .or. line == "q()") exit
      call eval_print(trim(line))
   end do
+  if (write_vars_at_end) then
+     open (newunit=varu, file=vars_file, action="write", status="replace")
+     do i=1,size(vars)
+        if (allocated(vars(i)%val)) write (varu, "(a,*(1x, f0.6))") &
+           trim(vars(i)%name), vars(i)%val
+     end do
+  end if
 end program xinterpret
 
