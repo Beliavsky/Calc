a8e6245e68a1b578e36a043debdffec95a2041fe
Beliavsky <beliavsky@aol.com>
2025-04-29 11:42:55 -0400

    replaces ** with ^ before sending to interpreter

--- xinterpret.f90
+++ xinterpret.f90
@@ -1,19 +1,28 @@
 program xinterpret
   use interpret_mod, only: eval_print, tunit, code_transcript_file, vars
+  use util_mod, only: replace
   implicit none
   integer :: i, iostat_err, varu, ipos_comment
   character (len=1000) :: line
   logical, parameter :: write_vars_at_end = .true.
   character (len=*), parameter :: vars_file = "temp_vars.txt", comment_char="!"
   open (newunit=tunit, file=code_transcript_file, action="write", status="replace")
-  call eval_print("n = 10")
-  call eval_print("y = [1, 2, 3]")
-  call eval_print("z = n * y")
-  call eval_print("w = [10 20 30] + y")
-  call eval_print("z ^ 2")
-  call eval_print("x = runif(n)")
-  call eval_print("sum(x)")
-  call eval_print("[sum(x) minval(x) maxval(x)]")
+  if (.true.) then
+     call eval_print("n = 10")
+     call eval_print("y = [1, 2, 3]")
+     call eval_print("z = n * y")
+     call eval_print("w = [10 20 30] + y")
+     call eval_print("z ^ 2")
+     call eval_print("x = runif(n)")
+     call eval_print("sum(x)")
+     call eval_print("[sum(x) minval(x) maxval(x)]")
+     call eval_print([character (len=999) :: "r = 10", "r^2", "r^3"])
+     call eval_print("v = 10*arange(10)")
+     call eval_print("v([2 3 4])")
+     call eval_print("v(3:9:2)")
+     call eval_print("rnorm(5)")
+     call eval_print("rnorm(10^3)")
+  end if
   do
      write (*,"('> ')", advance="no")
      read (*,"(a)", iostat=iostat_err) line
@@ -25,6 +34,7 @@ program xinterpret
      else if (ipos_comment > 0) then
         line = line(:ipos_comment-1)
      end if
+     line = replace(line, "**", "^")
      call eval_print(trim(line))
   end do
   if (write_vars_at_end) then
